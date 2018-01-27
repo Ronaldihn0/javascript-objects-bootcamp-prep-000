d247e78185352aa23b2deb0ba10e3cd62a186f5f
Ronald DeBeras <rdeberas@gmail.com>
2018-01-27 18:05:13 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = {'artistName': 'songTitle' }
 
 function updatePlaylist(object, key, value) {
-  return object.assign({}, object, {[key]: value})
+  return object.assign({}, object, { [key]: value})
 }
